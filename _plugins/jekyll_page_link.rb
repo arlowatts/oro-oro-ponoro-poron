71f930ed617ce3079ab2d98da2503af1ad5327e3
Arlo Watts <67204708+arlowatts@users.noreply.github.com>
2026-05-17 10:25:49 -0700

    Update plugin

--- _plugins/jekyll_page_link.rb
+++ _plugins/jekyll_page_link.rb
@@ -8,8 +8,8 @@ module Jekyll
 
         def render(context)
             site = context.registers[:site]
-            pages = site.pages.select {|page| page.registers[:data]["title"] == @text}
-            "[#{@text}](#{pages.first.path})"
+            pages = site.pages.select {|page| page.data["title"] == @text}
+            "[#{@text}](#{pages.first.permalink})"
         end
 
         Liquid::Template.register_tag("page_link", Jekyll::PageLink)
