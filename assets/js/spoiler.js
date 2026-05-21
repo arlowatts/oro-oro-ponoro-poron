1c520ee1e8c435cc2c52a5b67c73bfa74dd37a86
Arlo Watts <67204708+arlowatts@users.noreply.github.com>
2026-05-20 19:12:16 -0700

    Fix js

--- assets/js/spoiler.js
+++ assets/js/spoiler.js
@@ -3,6 +3,6 @@ addEventListener('load', main);
 function main() {
     if (location.toString().endsWith('?')) {
         for (element of document.getElementsByClassName('excerpt')) element.style.display = 'none';
-        for (element of document.getElementsByClassName('spoiler')) element.style.display = '';
+        for (element of document.getElementsByClassName('spoiler')) element.style.display = 'revert';
     }
 }
